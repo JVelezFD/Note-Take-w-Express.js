cfbbf18d4789f994bf62435363a3ac69ced5b4fe
JVelezFD <jvelez117@gmail.com>
2022-08-06 21:49:06 -0400

    adding delete code to the api route

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -35,9 +35,53 @@ const existingNote = fs.readFileSync(
             console.log(err)
         }
     }
-)
+);
+const notes = JSON.parse(existingNote);
+notes.push(userNote);
 
+fs.writeFile(
+    path.join(__dirname, '../../db/db.json'),
+    JSON.stringify(notes),
+    'utf-8',
+    (err) =>{
+        if(err){
+            console.log(err);
+        }
+    }
+);
+res.json({});
 }
+
+// Delete note the user selects
+
+function deleteNote (req, res){
+    const noteId = req.params.id;
+    const existingNote = fs.readFileSync (
+        path.join(__dirname, '../../db/db.json'),
+        'utf-8',
+        (err) => {
+            if (err) {
+                console.log(err);
+            }
+        }
+
+    );
+    //need to filter out the note by ID selecte by user
+    const notes = JSON.parse(existingNote);
+    const newNote = notes.filter((note) => note.id !== noteId);
+    fs.writeFile(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify(newNote),
+        'utf-8', 
+        (err) =>{
+        if(err){
+            console.log(err);
+        }
+        }
+    );
+    res.JSON({});
+}
+
 
 
 
