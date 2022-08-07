7d6ccbdb0c3bf3fc3b9a13c601d90e9476e128fd
JVelezFD <jvelez117@gmail.com>
2022-08-07 18:26:55 -0400

    added uuid script and code for unique id generator

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -1,95 +1,57 @@
-const express = require('express');
-const fs = require('fs');
-const router = require('express').router();
-const util = require('util');
-const path = require('path');
+const uuid = require('uuid');
+const uniqueRandomID = uuid.v4();
+
+const fs = require("fs");
+
+const editNote = (updatedNotesArray) => {
+  fs.writeFile("./db/db.json", JSON.stringify(updatedNotesArray), (err) => {
+    if (err) throw err;
+  });
+};
 
 
-//retrive notes on the DB and return the note for the client
+module.exports = (app) => {
+  // GET  pull existing notes
+  app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err; 
+      res.json(JSON.parse(data));
+    });
+  });
 
-function retriveNote(req, res){
-    const data = fs.readFile(
-        path.join(__dirname, '../../db/db.json'),
-        'utf-8',
-        (err) => {
-            if(err) {
-                console.log(err);
-            }
+  // POST user notes to DB
+
+  app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      const notesArr = JSON.parse(data);
+      newNote.id = uniqueRandomID({ length: 10 });
+      notesArr.push(newNote);
+      editNote(notesArr);
+      console.log(
+        `New Note Added! Title: ${JSON.stringify(
+          newNote.title
+        )}, Text: ${JSON.stringify(newNote.text)}, ID: ${newNote.id}`
+      );
+    res.send(notesArr);
+    });
+  });
+
+  // DELETE user note via unique ID
+  app.delete("/api/notes/:id", (req, res) => {
+    const deleteId = req.params.id;
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      let notesArr = JSON.parse(data);
+      for (let i = 0; i < notesArr.length; i++) {
+        if (notesArr[i].id === deleteId) {
+          notesArr.splice(i, 1);
         }
-    );
-    res.json({message: 'success note retrived'});
+      }
+      editNote(notesArr);
+      console.log(`Note Deleted! Note ID: ${deleteId}`);
+      res.send(notesArr);
+    });
+  });
 }
-
-//write the note to the DB from client/user input. Also give a unique ID for the note
-
-function writeNote (req, res) {
-    const userNote = req.body;
-    userNote.id = Math.floor(Math.random() *10000);
-
-// if there are existing notes adding the new notes to them
-
-const existingNote = fs.readFileSync(
-    path.join(__dirname, '../../db/db.json'),
-    'utf-8', 
-    (err) =>{
-        if(err)    {
-            console.log(err)
-        }
-    }
-);
-const notes = JSON.parse(existingNote);
-notes.push(userNote);
-
-fs.writeFile(
-    path.join(__dirname, '../../db/db.json'),
-    JSON.stringify(notes),
-    'utf-8',
-    (err) =>{
-        if(err){
-            console.log(err);
-        }
-    }
-);
-res.json({});
-}
-
-// Delete note the user selects
-
-function deleteNote (req, res){
-    const noteId = req.params.id;
-    const existingNote = fs.readFileSync (
-        path.join(__dirname, '../../db/db.json'),
-        'utf-8',
-        (err) => {
-            if (err) {
-                console.log(err);
-            }
-        }
-
-    );
-    //need to filter out the note by ID selecte by user
-    const notes = JSON.parse(existingNote);
-    const newNote = notes.filter((note) => note.id !== noteId);
-    fs.writeFile(
-        path.join(__dirname, '../../db/db.json'),
-        JSON.stringify(newNote),
-        'utf-8', 
-        (err) =>{
-        if(err){
-            console.log(err);
-        }
-        }
-    );
-    res.JSON({});
-}
-
-//post, delete, get routes for router
-
-router.post('/notes',  writeNote);
-
-router.get('/notes', existingNote);
-
-router.delete('/notes', deleteNote);
-
-module.exports = router;
-
