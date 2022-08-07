fff0492e92a87711193f2cec5fa1bf81216abacb
JVelezFD <jvelez117@gmail.com>
2022-08-06 21:20:30 -0400

    adding js for retriving and writing to DB

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -4,9 +4,41 @@ const router = require('express').router();
 const util = require('util');
 const path = require('path');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//retrive notes on the DB and return the note for the client
 
-app.use(express.static('public'));
+function retriveNote(req, res){
+    const data = fs.readFile(
+        path.join(__dirname, '../../db/db.json'),
+        'utf-8',
+        (err) => {
+            if(err) {
+                console.log(err);
+            }
+        }
+    );
+    res.json({message: 'success note retrived'});
+}
+
+//write the note to the DB from client/user input. Also give a unique ID for the note
+
+function writeNote (req, res) {
+    const userNote = req.body;
+    userNote.id = Math.floor(Math.random() *10000);
+
+// if there are existing notes adding the new notes to them
+
+const existingNote = fs.readFileSync(
+    path.join(__dirname, '../../db/db.json'),
+    'utf-8', 
+    (err) =>{
+        if(err)    {
+            console.log(err)
+        }
+    }
+)
+
+}
+
+
 
 
--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const apiRoutes = require('./routes/api');
 const homeRoutes = require('./routes/homeRoutes');
+const util = require('util');
 
 
 app.use(express.json());
